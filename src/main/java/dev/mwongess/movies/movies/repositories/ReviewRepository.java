536e911279ebecd9d1d37708da49058b85de7956
mwongess <amosmwongelah@gmail.com>
2024-07-22 17:11:01 +0300

    create reviews api for movies

---
+++ src/main/java/dev/mwongess/movies/movies/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.mwongess.movies.movies.repositories;
+
+import dev.mwongess.movies.movies.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
